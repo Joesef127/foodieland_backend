fa780a88488197b036c93c12cd6596365dbbd980
joesef127 <adegboladayor@gmail.com>
2025-05-31 18:17:52 +0100

    chore: edited cors

--- app/main.py
+++ app/main.py
@@ -9,10 +9,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5173",
-        "http://localhost:5173",
-        "https://foodielandweb.vercel.app",
-        "https://foodieland-tau.vercel.app"
+        "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
